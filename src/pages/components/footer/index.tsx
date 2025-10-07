10f7d2e2459c170dcb65ed3bf68522db7184fc11
RCA04 <ryan04.dev@gmail.com>
2025-10-07 17:41:45 -0300

    develop: componente footer

---
+++ src/pages/components/footer/index.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import Link from "next/link"
+import { BsWhatsapp } from "react-icons/bs"
+import { CiLinkedin } from "react-icons/ci"
+import { FaGithub} from "react-icons/fa"
+import { IoIosMail } from "react-icons/io"
+
+
+
+
+
+
+export default function Footer() {
+    return(
+        <div className="w-full">
+
+            <div className="flex gap-10 w-full mt-3 justify-center text-2xl">
+                    <div className=" gap-1 flex items-center">
+                    <CiLinkedin/>
+                    <Link href='https://www.linkedin.com/in/ryan-costa04'>Ryan da Costa araújo</Link>
+                    </div>
+                    
+                    <div className=" gap-1 flex items-center">
+                    <FaGithub/>
+                    <Link href='https://github.com/RCA04'>RCA04</Link>
+                    </div>
+                    
+                    <div className="gap-1 flex items-center">
+                    <IoIosMail/>
+                    <p>ryan04.dev@gmail.com</p>
+                    </div>
+                    
+                    <div className="gap-1 flex items-center">
+                    <BsWhatsapp/>
+                    <Link href='https://wa.me/5561985821807'>Fale comigo</Link>
+                    </div>
+            </div>
+
+            <div className="flex justify-center items-center mt-5 h-15">
+                <button className="border-1 border-slate-300 w-1/2 h-15 cursor-pointer
+                 text-xl font-bold rounded-4xl
+                 hover:bg-slate-300 hover:mb-3
+                 hover:shadow-2xl/50 
+                 duration-200 ease-in
+                 hover:scale-105
+                 ">
+                Enviar Email
+                </button>
+            </div>
+
+        </div>
+    )
+
+}
