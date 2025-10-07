5694197e64ac2a63903a7be5fe517c2397450289
RCA04 <ryan04.dev@gmail.com>
2025-10-07 14:43:41 -0300

    develop: finalização da lógica e DOM do componente

--- src/pages/components/knowledge/index.tsx
+++ src/pages/components/knowledge/index.tsx
@@ -9,6 +9,7 @@ import { GrMysql } from "react-icons/gr";
 import { IoGitBranchOutline } from "react-icons/io5";
 import { ImNpm } from "react-icons/im";
 import { FaUbuntu } from "react-icons/fa6";
+import { DiJavascript } from "react-icons/di";
 
 export default function  Knowledge(){
 
@@ -16,8 +17,26 @@ export default function  Knowledge(){
 
 
     const descriptions: Record<string, string>={
-      html:,
-
+      html:"Utilizo como base para React, Next, e Laravel, Php",
+      css:"Utilizo como base para projetos e para bibliotecas como tailwind e styled components",
+      javaScript:"utilizo como base para o React e Next, além de usar como alternativas grosseiras para soluções",
+      reactjs:"Uso React para criar aplicações SPA(single page aplication)",
+      nextjs:"Uso next para criar aplicações SPA(single page aplication), SSR(server side render), CSR(client side render) e gosto dele devido a perfomace que é fornecida",
+      styledcomponents:"Gosto de utilizar StyledComponents quando vou ter componentes iguais em várias páginas devido ao seu fácil reaproveitamento",
+      tailwind:"Utilizo Tailwind nas aplicações quando quero ter facilidade de tornar uma aplicação responsiva",
+      mui:"Gosto de usar o Mui quando vou fazer uma aplicação que precisa de responsividade e padronização, poís ele facilita ter ambos",
+      php:"Uso de base para Laravel quando vou fazer uma aplicação monolitica ou o back-end de outra aplicação",
+      laravel:"Gosto de utilizar o laravel para fazer o back-end das aplicações, além dele base também uso junto ao Laravel:sanctum para realizar as autenticações",
+      webtoken:"Sempre uso o JSON-Webtoken para fazer as autenticações amarzenado ele no localStorage",
+      mysql:"Utilizo o MySQL para realizar as consultas de dados quando necessário no banco",
+      git:"Utilizo o git para ter um controle maior sobre as versões do meu código",
+      gitflow:"Complemento do git, que uso para ter um Controle ainda maior das versões além de um fluxo visual mais compreensível",
+      npm:"Uso como padrão para gerenciar os pacotes das minha aplicações",
+      yarn:"Uso quando necessido que o gerenciamento de pacotes seja mais veloz",
+      composer:"Utilizo como gerenciador de dependências do PHP/Laravel",
+      eslint:"Sempre antes de commitar um código utilizo o Eslint para ter um código mais limpo",
+      prettier:"Uso normalmente proximo a conclusão de um projeto para deixa-lo mais légivel",
+      ubuntu:"Uso o Ubuntu como sistema operacional padrão quando vou programar, gosto devido a sua leveza e facilidade de gerenciamento de configuracções do sistema.",
     };
 
     return(
@@ -30,61 +49,137 @@ export default function  Knowledge(){
             {desc === "html" && <div>{descriptions.html}</div>}
           </div>
           <div>
-            <FaCss3/>
+            <button onClick={()=>setDesc(desc === "css" ? null : "css")}>
+                <FaCss3/>
+            </button>
+            {desc === "css" && <div>{descriptions.css}</div>}
+            
           </div>
           <div>
-              <IoLogoJavascript/>
+                        <button onClick={()=>setDesc(desc === "javaScript" ? null : "javaScript")}>
+                <IoLogoJavascript/>
+            </button>
+            {desc === "javaScript" && <div>{descriptions.javaScript}</div>}
+              
           </div>
           <div>
-              <FaReact/>
+                        <button onClick={()=>setDesc(desc === "reactjs" ? null : "reactjs")}>
+                <FaReact/>
+            </button>
+            {desc === "reactjs" && <div>{descriptions.reactjs}</div>}
+              
           </div>
           <div>
-            <RiNextjsLine/>
+                        <button onClick={()=>setDesc(desc === "nextjs" ? null : "nextjs")}>
+                <RiNextjsLine/>
+            </button>
+            {desc === "nextjs" && <div>{descriptions.nextjs}</div>}
+            
           </div>
           <div>
-              <SiStyledcomponents/>
+                        <button onClick={()=>setDesc(desc === "styledcomponents" ? null : "styledcomponents")}>
+                <SiStyledcomponents/>
+            </button>
+            {desc === "styledcomponents" && <div>{descriptions.styledcomponents}</div>}
+              
           </div>
           <div>
-              <RiTailwindCssFill/>
+                        <button onClick={()=>setDesc(desc === "tailwind" ? null : "tailwind")}>
+                <RiTailwindCssFill/>
+            </button>
+            {desc === "tailwind" && <div>{descriptions.tailwind}</div>}
+              
           </div>
           <div>
-              <SiMui/>
+                        <button onClick={()=>setDesc(desc === "mui" ? null : "mui")}>
+                <SiMui/>
+            </button>
+            {desc === "mui" && <div>{descriptions.mui}</div>}
+              
           </div>
           <div> 
-              <FaPhp/>
+                        <button onClick={()=>setDesc(desc === "php" ? null : "php")}>
+                <FaPhp/>
+            </button>
+            {desc === "php" && <div>{descriptions.php}</div>}
+              
           </div>  
           <div>
-              <FaLaravel/>
+                        <button onClick={()=>setDesc(desc === "laravel" ? null : "laravel")}>
+                <FaLaravel/>
+            </button>
+            {desc === "laravel" && <div>{descriptions.laravel}</div>}
+              
           </div>
           <div>
-              <SiJsonwebtokens/>
+                        <button onClick={()=>setDesc(desc === "webtoken" ? null : "webtoken")}>
+                <SiJsonwebtokens/>
+            </button>
+            {desc === "webtoken" && <div>{descriptions.webtoken}</div>}
+              
           </div>
           <div>
-              <GrMysql/>
+                        <button onClick={()=>setDesc(desc === "mysql" ? null : "mysql")}>
+                <GrMysql/>
+            </button>
+            {desc === "mysql" && <div>{descriptions.mysql}</div>}
+              
           </div>
           <div>
-            <FaGitAlt/>
+                        <button onClick={()=>setDesc(desc === "git" ? null : "git")}>
+                <FaGitAlt/>
+            </button>
+            {desc === "git" && <div>{descriptions.git}</div>}
+            
           </div>
           <div>
-            <IoGitBranchOutline/>
+                        <button onClick={()=>setDesc(desc === "gitflow" ? null : "gitflow")}>
+                <IoGitBranchOutline/>
+            </button>
+            {desc === "gitflow" && <div>{descriptions.gitflow}</div>}
+            
           </div>
           <div>
-            <ImNpm/>
+                        <button onClick={()=>setDesc(desc === "npm" ? null : "npm")}>
+                <ImNpm/>
+            </button>
+            {desc === "npm" && <div>{descriptions.npm}</div>}
+            
           </div>
           <div>
-              <FaYarn/>
+                        <button onClick={()=>setDesc(desc === "yarn" ? null : "yarn")}>
+                <FaYarn/>
+            </button>
+            {desc === "yarn" && <div>{descriptions.yarn}</div>}
+              
           </div>
           <div>
-            <SiComposer/>
+                        <button onClick={()=>setDesc(desc === "composer" ? null : "composer")}>
+                <SiComposer/>
+            </button>
+            {desc === "composer" && <div>{descriptions.composer}</div>}
+            
           </div>
           <div>
-            <SiEslint/>
+                        <button onClick={()=>setDesc(desc === "eslint" ? null : "eslint")}>
+                <SiEslint/>
+            </button>
+            {desc === "eslint" && <div>{descriptions.eslint}</div>}
+            
           </div>
           <div>
-            <SiPrettier/>
+                        <button onClick={()=>setDesc(desc === "prettier" ? null : "prettier")}>
+                <SiPrettier/>
+            </button>
+            {desc === "prettier" && <div>{descriptions.prettier}</div>}
+            
           </div>
           <div>
-            <FaUbuntu/>
+                        <button onClick={()=>setDesc(desc === "ubuntu" ? null : "ubuntu")}>
+                <FaUbuntu/>
+            </button>
+            {desc === "ubuntu" && <div>{descriptions.ubuntu}</div>}
+            
           </div>
         </div>
         
