0577580dd9546882b83161d7ad09e2ce58559481
RCA04 <ryan04.dev@gmail.com>
2026-02-04 17:36:18 -0300

    google fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+      <meta name="google-site-verification" content="KLIxhVDN-oTnzrDJ_Ac83ifEoe4lfAvYy-jH1uaWYlQ" />
+      </Head>
+
       <body className=" text-slate-100">
         <Main />
         <NextScript />
